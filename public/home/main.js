4012f8aa50efc40ffcb3866935ea2df0fca761c7
Thyagaraj <valane91@gmail.com>
2018-09-30 20:45:16 +0200

    Add a dropdown to see JSON count information of a choosen port

--- public/home/main.js
+++ public/home/main.js
@@ -1,5 +1,11 @@
 
-
+/*
+ * 
+ * Load the count informations in the home page.
+ * Fetch data from '/count_info' which is a json sent by the server 
+ * with the count informations of the current port. 
+ *  
+ */ 
 function loadCountInfo() {
 
     fetch('count_info')
@@ -21,4 +27,35 @@ function loadCountInfo() {
         });
 }
 
+/*
+ * 
+ * Load the ports list in the home page's element #ports-list. 
+ * Fetch data from '/ports_list' which is a json sent by the server 
+ * with the list of all the ports that has been open. 
+ * 
+ */ 
+function loadPortsList() {
+
+    fetch('ports_list')
+        .then(response => {
+
+            if(response.status === 200) {
+                return response.json(); 
+            } else {
+                throw new Error(`request error: ${response.data}`); 
+            }
+        })
+        .then(data => {
+            for(const port in data) {
+                let portElt = document.createElement("li"); 
+                portElt.innerHTML = `<a href="count_requests/${port}" >${port}</a>`; 
+
+                document.querySelector("#ports-list").appendChild(portElt); 
+            }
+        })
+}
+
+// Call out loadCountInfo so that it initialize #current-count and #total-count. 
 loadCountInfo(); 
+// Call out loadPortsList so that it initialize #ports-list.
+loadPortsList(); 
--- public/port_info/port_count_info.js
+++ public/port_info/port_count_info.js
@@ -1,15 +1,36 @@
+
+/*
+ * 
+ * Retrieve the port number from the uri 
+ * of the current page, and add it to countInfoURI. 
+ * Doesn't modify countInfoURI if there isn't a port id in the URI. 
+ * 
+ * @param countInfoURI, the uri to parsse
+ *  
+ */
+const getPortId = (countInfoURI) => {
+    const uri = window.location.pathname;
+    const portId = uri.split('/')[uri.length - 1]; 
+
+    if(isNaN(portId)) {
+        countInfoURI += ('/' + portId); 
+    }
+}
+
+
+
+/*
+ * 
+ * Load the count informations in the home page.
+ * Fetch data from '/count_info' which is a json sent by the server 
+ * with the count informations of the current port. 
+ *  
+ */ 
 function loadJSONPortCountInfo() {
 
     let countInfoURI = '/count_info'; 
+    getPortId(countInfoURI);
 
-    const getPortId = () => {
-        const uri = window.location.pathname;
-        const portId = uri.split('/')[uri.length - 1]; 
-
-        if(isNaN(portId)) {
-            countInfoURI += ('/' + portId); 
-        }
-    }
 
     fetch(countInfoURI)
         .then(response => {
